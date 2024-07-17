50e3e3b29c60375232256095884527e1a19f292c
Kehvarl <kehvarl@kehvarl.com>
2024-07-16 19:42:14 -0700

    Using the Entity Class to draw more

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,10 @@
 require('app/entity.rb')
 
 def tick args
-  args.state.player ||= Entity.new(x=640,y=360,char=[0,64],r=255,g=255,b=0)
+  args.state.player ||= Entity.new(x=40,y=20,char=[0,64],r=255,g=255,b=255)
+  args.state.entities ||= [args.state.player, Entity.new(x=42,y=20,char=[0,64],r=255,g=255,b=0)]
   args.outputs.primitives << {x:0, y:0, w:1280, h:720, r:0, g:0, b:0}.solid!
-  args.outputs.primitives << args.state.player
+  args.outputs.primitives << args.state.entities
 
   if args.inputs.keyboard.key_down.up
     args.state.player.move(0,1)
