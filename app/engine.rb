f0fb5e321b87ffb791aa8636258be1bb59635b07
Kehvarl <kehvarl@kehvarl.com>
2024-09-25 21:23:13 -0700

    Enemy Turns

--- app/engine.rb
+++ app/engine.rb
@@ -6,25 +6,35 @@ class Engine
     @game_map.do_fov(@player.pos_x, @player.pos_y)
   end
 
+  def handle_enemy_turns()
+    @game_map.entities.select{|e| e.name!="Player"}.each do |e|
+      puts("#{e.name} wonders when it will get to take a real turn")
+    end
+  end
+
   def handle_events(events)
-      events.each do |event|
-        if event.type == :player_move
-          r = @player.get_potential_move(event.dx, event.dy)
-          if @game_map.valid_move(r[0], r[1])
-            e = @game_map.get_blocking_entity_at(r[0], r[1])
-            if e == []
-              @player.move(event.dx, event.dy)
-            else
-              events << {type: :attack, x: e.pos_x, y: e.pos_y}
-              # melee action
-            end
-            @game_map.do_fov(@player.pos_x, @player.pos_y, 10)
+    if events == []
+      return
+    end
+    events.each do |event|
+      if event.type == :player_move
+        r = @player.get_potential_move(event.dx, event.dy)
+        if @game_map.valid_move(r[0], r[1])
+          e = @game_map.get_blocking_entity_at(r[0], r[1])
+          if e == []
+            @player.move(event.dx, event.dy)
+          else
+            events << {type: :attack, x: e.pos_x, y: e.pos_y}
+            # melee action
           end
-        elsif event.type == :attack
-          e = @game_map.get_blocking_entity_at(event.x, event.y)
-          puts ("Destination is blocked by Entity: #{e.name}")
+          @game_map.do_fov(@player.pos_x, @player.pos_y, 10)
         end
+      elsif event.type == :attack
+        e = @game_map.get_blocking_entity_at(event.x, event.y)
+        puts ("Destination is blocked by Entity: #{e.name}")
       end
+    end
+    handle_enemy_turns()
   end
 
   def render
